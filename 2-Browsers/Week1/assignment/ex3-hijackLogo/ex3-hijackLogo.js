a1d4cd9a7db4b7b2be7621a13d69b054d051cb46
Majd Hamde <majdhamde1901@gmail.com>
2025-09-18 01:26:30 +0200

    edited everything

---
+++ 2-Browsers/Week1/assignment/ex3-hijackLogo/ex3-hijackLogo.js
@@ -0,0 +1,23 @@
+/*------------------------------------------------------------------------------
+Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-Browsers/Week1#exercise-3-the-logo-hijack
+
+1. Find out how to select the element that contains the Google logo, and store 
+   it in a variable.
+2. Modify the `src` and `srcset` of the logo so that it's replaced by the 
+   HackYourFuture logo instead.
+------------------------------------------------------------------------------*/
+function hijackGoogleLogo() {
+  // 1. Select the Google logo
+  const logo = document.querySelector('img');
+
+  if (logo) {
+    // 2. Replace the src and srcset with HackYourFuture logo
+    logo.src = 'assets/hyf-logo-black-bg-small.png';
+    logo.srcset = '';
+  } else {
+    console.log('Google logo not found!');
+  }
+}
+
+// Execute
+hijackGoogleLogo();
